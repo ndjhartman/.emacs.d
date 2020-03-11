83b8321591760eaec745e03cd65551a4c268bdc4
Nathan Hartman <hnathan918@gmail.com>
2020-03-10 21:55:10 -0400

    Removed pyim as not needed

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Thu Mar 14 10:15:28 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Tue Mar 10 21:24:22 2020 (-0400)
+;; Last-Updated: Tue Mar 10 21:54:22 2020 (-0400)
 ;;           By: Nathan Hartman
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d init
@@ -217,8 +217,6 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
 (require 'init-pdf)
 
 (require 'init-leetcode)
-
-(require 'init-pyim)
 
 (require 'init-epaint)
 
